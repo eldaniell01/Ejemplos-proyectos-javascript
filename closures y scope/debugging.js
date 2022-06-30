ddd2f6de7624aad0e76da103062d2a3da6a1ac69
eldaniell01 <amontepequem@miumg.edu.gt>
2022-06-29 19:42:27 -0600

    clousures y scope 3

---
+++ closures y scope/debugging.js
@@ -0,0 +1,10 @@
+var a = 'hello';
+
+function hello(){
+    let b = 'hello 2';
+    const c = 'hello 3';
+    debugger;
+}
+
+hello();
+// herramienta para inspeccionar errores
---
+++ closures y scope/hoisting.js
@@ -0,0 +1,5 @@
+// las variables se asignan al principio con undefined, aplica lo mismo para las funciones
+//lo que se declara de utimo, se vuelve a declarar al principio. 
+console.log(name2);
+var name2 = 'el';
+console.log(name2);
