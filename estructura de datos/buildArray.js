56dd0a2f942445d3f47145020b3378ad1e92e8b2
eldaniell01 <amontepequem@miumg.edu.gt>
2022-08-08 20:05:12 -0600

    buildhashtable

--- estructura de datos/buildArray.js
+++ estructura de datos/buildArray.js
@@ -19,6 +19,27 @@ class Myarray{
         this.length--;
         return ultimoItem;
     }
+    del(index){
+        const item = this.data[index];
+        this.ind(index);
+        return item;
+    }
+
+    ind(index){
+        for(let i = index; i<this.length-1;i++){
+            this.data[i] =this.data[i+1];
+        }
+        delete this.data[this.length-1];
+        this.length--;
+    }
+    pushi(item){
+        this.length++;
+        for(let i = this.length; i>0; i--){
+            this.data[i] = this.data[i-1];
+        }
+        this.data[0] = item;
+        return this.data;
+    }
 }
 
 const myclass = new Myarray();
